12225f24d4983c55da57106382f70b97ffb007fb
Wang Siyuan <wsy0227@sjtu.edu.cn>
2024-08-18 22:33:29 +0800

    Update ruler_installation.sh

--- docker/ruler_installation.sh
+++ docker/ruler_installation.sh
@@ -22,7 +22,7 @@ pip install packaging
 
 echo "Installing the required Python packages..."
 # pip freeze | grep torch >> requirements.txt
-pip install -r requirements.txt
+pip install -r requirements.txt --no-deps
 
 echo "Installing the TransformerEngine package..."
 
