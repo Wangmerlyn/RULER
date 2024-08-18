9845de4ffa1bf1da9a9c271d6d1ae6e22353e528
Wang Siyuan <wsy0227@sjtu.edu.cn>
2024-08-18 22:59:37 +0800

    Update ruler_installation.sh

--- docker/ruler_installation.sh
+++ docker/ruler_installation.sh
@@ -24,6 +24,19 @@ echo "Installing the required Python packages..."
 # pip freeze | grep torch >> requirements.txt
 pip install -r requirements.txt 
 
+echo "reinstalling torch"
+
+if [[ "$cuda_version" == "11.8" ]]; then
+    echo "Installing PyTorch 2.1.0 for CUDA 11.8..."
+    pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu118
+elif [[ "$cuda_version" == "12.2" ]]; then
+    echo "Installing the latest PyTorch version for CUDA 12.2..."
+    pip install torch torchvision torchaudio
+else
+    echo "Installing the latest PyTorch version for CUDA ${cuda_version}... though it may not be compatible."
+    pip install torch torchvision torchaudio
+fi
+
 echo "Installing the TransformerEngine package..."
 
 # pip install git+https://github.com/NVIDIA/TransformerEngine.git@stable
@@ -41,6 +54,8 @@ pip install tqdm
 pip install hydra-core
 pip install omegaconf
 pip install pytorch-lightning 
+pip install numpy==1.23.5 --no-deps
+pip install huggingface_hub==0.23.2 --no-deps
 
 
 
